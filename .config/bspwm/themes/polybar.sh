a9f9b7a83089e7de25b65d4652b80e29c1b56528
pabrax <pabloeg0420@outlook.com>
2024-07-22 22:46:53 -0500

    remove wallpapers folder (other repository)

--- .config/bspwm/themes/polybar.sh
+++ .config/bspwm/themes/polybar.sh
@@ -6,7 +6,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SFILE="$DIR/system.ini"
 RFILE="$DIR/.system"
-STYLE="dark_horizon"
+STYLE="black_metal"
 
 ## Get system variable values for various modules
 get_values() {
